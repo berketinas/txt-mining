e3f91deeb61256d06ffa0d5fbb1f12c341a914d2
Berke Tınas <berke_750@hotmail.com>
2022-12-18 18:29:48 +0300

    BoW and TF-IDF models

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ from nltk.corpus import stopwords
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-pd.set_option('display.max_colwidth', 200)
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+from IPython.display import display
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
 POS_DATA_PATH = './dataset/pos'
 NEG_DATA_PATH = './dataset/neg'
@@ -45,4 +52,21 @@ for file in os.listdir(NEG_DATA_PATH):
     FILE_PATH = f"{NEG_DATA_PATH}\{file}"
     reviews_clean_df = pd.concat([reviews_clean_df, read(FILE_PATH)], axis=0, ignore_index=True)
 
-print(reviews_clean_df)
+bow_model = CountVectorizer(max_features=10)
+bow_df = pd.DataFrame(bow_model.fit_transform(reviews_clean_df['cleaned_text']).todense())
+bow_df.columns = sorted(bow_model.vocabulary_)
+
+display(bow_df)
+
+tfidf_model = TfidfVectorizer(max_features=10)
+tfidf_df = pd.DataFrame(tfidf_model.fit_transform(reviews_clean_df['cleaned_text']).todense())
+tfidf_df.columns = sorted(tfidf_model.vocabulary_)
+
+display(tfidf_df)
+
+# rw = 8
+# print(list(bow_df.columns[bow_df.iloc[rw, :] == bow_df.iloc[rw, :].max()]))
+# print(list(tfidf_df.columns[tfidf_df.iloc[rw, :] == tfidf_df.iloc[rw, :].max()]))
+#
+# print(bow_df[bow_df['life']!=0].shape[0])
+# print(bow_df[bow_df['like']!=0].shape[0])
