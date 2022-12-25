5d1e25069025c53114bb0b4762a999a1b37a5488
Berke Tınas <berke_750@hotmail.com>
2022-12-25 15:32:11 +0300

    feature extraction: polarity

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 
+from textblob import TextBlob
+
 from IPython.display import display
 
 pd.set_option('display.max_rows', 500)
@@ -26,6 +28,8 @@ stop_words = stopwords.words('english')
 stop_words = stop_words + list(string.printable)
 
 reviews_clean_df = pd.DataFrame(columns=['cleaned_text'])
+
+MAX_FEATURES = 10
 
 
 def read(file_path):
@@ -44,6 +48,47 @@ def preprocess(review):
     return temp_clean_df
 
 
+def b2models(reviews):
+    bow_model = CountVectorizer(max_features=MAX_FEATURES)
+    bow_df = pd.DataFrame(bow_model.fit_transform(reviews['cleaned_text']).todense())
+    bow_df.columns = sorted(bow_model.vocabulary_)
+
+    display(bow_df)
+
+    tfidf_model = TfidfVectorizer(max_features=MAX_FEATURES)
+    tfidf_df = pd.DataFrame(tfidf_model.fit_transform(reviews['cleaned_text']).todense())
+    tfidf_df.columns = sorted(tfidf_model.vocabulary_)
+
+    display(tfidf_df)
+
+
+def feature_extraction(reviews):
+    reviews['polarity'] = reviews['cleaned_text'].apply(lambda x: str(TextBlob(x).sentiment.polarity))
+    display(reviews)
+
+    return reviews
+
+
+def polarity_test(reviews):
+    sum_pos_polarity = 0
+    sum_neg_polarity = 0
+    sum_false = 0
+
+    for iterator in range(2000):
+        if float(reviews.iloc[iterator, 1]) > 0.09 and iterator <= 1000:
+            sum_pos_polarity += 1
+
+        elif float(reviews.iloc[iterator, 1]) < 0.09 and iterator > 1000:
+            sum_neg_polarity += 1
+
+        else:
+            sum_false += 1
+
+    print('sum_pos_polarity: ', sum_pos_polarity)
+    print('sum_neg_polarity: ', sum_neg_polarity)
+    print('sum_false: ', sum_false)
+
+
 for file in os.listdir(POS_DATA_PATH):
     FILE_PATH = f"{POS_DATA_PATH}\{file}"
     reviews_clean_df = pd.concat([reviews_clean_df, read(FILE_PATH)], axis=0, ignore_index=True)
@@ -52,21 +97,7 @@ for file in os.listdir(NEG_DATA_PATH):
     FILE_PATH = f"{NEG_DATA_PATH}\{file}"
     reviews_clean_df = pd.concat([reviews_clean_df, read(FILE_PATH)], axis=0, ignore_index=True)
 
-bow_model = CountVectorizer(max_features=10)
-bow_df = pd.DataFrame(bow_model.fit_transform(reviews_clean_df['cleaned_text']).todense())
-bow_df.columns = sorted(bow_model.vocabulary_)
+reviews_clean_df = feature_extraction(reviews_clean_df)
 
-display(bow_df)
 
-tfidf_model = TfidfVectorizer(max_features=10)
-tfidf_df = pd.DataFrame(tfidf_model.fit_transform(reviews_clean_df['cleaned_text']).todense())
-tfidf_df.columns = sorted(tfidf_model.vocabulary_)
 
-display(tfidf_df)
-
-# rw = 8
-# print(list(bow_df.columns[bow_df.iloc[rw, :] == bow_df.iloc[rw, :].max()]))
-# print(list(tfidf_df.columns[tfidf_df.iloc[rw, :] == tfidf_df.iloc[rw, :].max()]))
-#
-# print(bow_df[bow_df['life']!=0].shape[0])
-# print(bow_df[bow_df['like']!=0].shape[0])
